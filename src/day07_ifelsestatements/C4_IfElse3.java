8459ebf02c37c1072608e2768023b234fed4ad49
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-02-20 19:36:30 +0300

    7.gun ara

--- src/day07_ifelsestatements/C4_IfElse3.java
+++ src/day07_ifelsestatements/C4_IfElse3.java
@@ -6,13 +6,13 @@ public class C4_IfElse3 {
 
 	public static void main(String[] args) {
 		// kullanicidan yasini sorun
-		// 65 den buyukse "emekli olabilirsin" yoksa "emekli olamazsin" yazdirin
+		// yas 65 veya buyukse "emekli olabilirsin" yoksa "emekli olamazsin" yazdirin
 		
 		Scanner scan =new Scanner(System.in);
 		System.out.println("Lutfen yasinizi giriniz");
 		int yas=scan.nextInt();
 		
-		if (yas>65) {
+		if (yas>=65) {
 			System.out.println("Emekli Olabilirsin");
 		} else {
 			System.out.println("Emekli olamazsin");
--- src/day07_ifelsestatements/C5_IfElse4.java
+++ src/day07_ifelsestatements/C5_IfElse4.java
@@ -1,9 +1,26 @@
 package day07_ifelsestatements;
 
+import java.util.Scanner;
+
 public class C5_IfElse4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Kullanicidan ucgenin kenar uzunluklarini alin
+		// uc kenar esitse "eskenar" yazdirin
+		// yoksa "eskenar degil" yazdirin
+		
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Lutfen ucgenin kenarlarini girin");
+		double kenar1=scan.nextDouble();
+		double kenar2=scan.nextDouble();
+		double kenar3=scan.nextDouble();
+		
+		if (kenar1==kenar2 && kenar2==kenar3) {
+			System.out.println("eskenar ucgen");
+		} else {
+			System.out.println("eskenar degil");
+		}
+		
 
 	}
 
