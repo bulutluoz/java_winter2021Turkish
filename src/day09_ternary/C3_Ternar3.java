ba77dd9aeec53c405abe6cd7f7750834b6f9e1db
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-02-23 23:59:34 +0300

    ternary

--- src/day09_ternary/C3_Ternar3.java
+++ src/day09_ternary/C3_Ternar3.java
@@ -1,10 +1,22 @@
 package day09_ternary;
 
+import java.util.Scanner;
+
 public class C3_Ternar3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Soru1 ) Kullanicidan iki sayi alin ve buyuk olan sayiyi yazdirin
 
+
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Lutfen karsilastirmak icin iki sayi giriniz");
+		
+		double sayi1=scan.nextDouble();
+		double sayi2=scan.nextDouble();
+		
+		
+		System.out.println(sayi1>sayi2 ? sayi1 : sayi2 );
+		
 	}
 
 }
---
+++ src/day09_ternary/C4_Ternary4.java
@@ -0,0 +1,26 @@
+package day09_ternary;
+
+import java.util.Scanner;
+
+public class C4_Ternary4 {
+
+	public static void main(String[] args) {
+		// Soru2 ) Kullanicidan bir tamsayi alin ve 
+		// sayinin tek veya cift oldugunu yazdirin 
+		
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Lutfen bir tam sayi giriniz");
+		
+		int sayi=scan.nextInt();
+		
+		System.out.println(sayi%2==0 ? "cift sayi" : "teksayi");
+		
+		// girilen sayinin mutlak degerini yazdiriniz
+		// 5 ->5    -12 -> 12  (girilen sayi x olsun . x pozitifse |x|=x  x negatifse |x|=-x )
+		
+		System.out.println(sayi>0 ? sayi : -sayi);
+		
+		
+	}
+
+}
