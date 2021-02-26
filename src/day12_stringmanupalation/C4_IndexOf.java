78ab1288fd40d5730cbc8ba2746f377f838e151c
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-02-27 00:26:09 +0300

    12.ders sonu

--- src/day12_stringmanupalation/C4_IndexOf.java
+++ src/day12_stringmanupalation/C4_IndexOf.java
@@ -1,5 +1,7 @@
 package day12_stringmanupalation;
 
+import java.util.Scanner;
+
 public class C4_IndexOf {
 
 	public static void main(String[] args) {
@@ -21,6 +23,26 @@ public class C4_IndexOf {
 		
 		System.out.println("olmayan harf " + str.indexOf("dert"));// 
 		
+		// kullanicidan bir cumle isteyin
+		// case sensitive olmadan Java kelimesi icerip icermedigini yazdirin
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Lutfen bir cumle giriniz: ");
+		
+		String cumle = scan.nextLine().toLowerCase();
+		
+		//int sonuc = cumle.indexOf("java"); // java varsa, ilk java kelimesinin index'ini dondurur
+										   // java yoksa, -1
+		
+		//System.out.println(sonuc==-1 ? "Java icermiyor" : "Java iceriyor");
+		
+		
+		if (cumle.indexOf("java")>=0) { // java kelimesinin index'inin 0 veya daha buyuk old.kon.ediyor
+			
+			System.out.println("Cumle java iceriyor");
+		} else {
+			System.out.println("Cumle Java icermiyor");
+		}
 
 	}
 
---
+++ src/day12_stringmanupalation/C5_LastIndexOf.java
@@ -0,0 +1,37 @@
+package day12_stringmanupalation;
+
+import java.util.Scanner;
+
+public class C5_LastIndexOf {
+
+	public static void main(String[] args) {
+		// Kullanicidan bir cumle alin
+		// ve asagidaki uc durumdan uygun olani yazdirin
+		// 1- cumle java icermiyor
+		// 2- cumle 1 tane java iceriyor
+		// 3- cumlede birden fazla java var
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Lutfen bir cumle giriniz: ");
+		
+		String cumle = scan.nextLine().toLowerCase();
+		
+		int ilkJava=cumle.indexOf("java"); // -1 yada ilk java'nin index'ini
+		int sonJava=cumle.lastIndexOf("java"); // -1 yada son java'nin index'ini
+		
+		if (ilkJava==-1) {
+			
+			System.out.println("cumle java icermiyor");
+			
+		} else if(ilkJava==sonJava){
+			System.out.println("cumle 1 java kelimesi iceriyor");
+
+		} else {
+			System.out.println("cumle 1'den fazla java iceriyor");
+		}
+		
+		
+
+	}
+
+}
