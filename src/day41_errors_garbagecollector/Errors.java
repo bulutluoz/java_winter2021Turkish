a67cdbe5282c363d438cac6ab76ede7e5fa532d4
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-04-01 22:01:54 +0300

    41.ders ara

---
+++ src/day41_errors_garbagecollector/Errors.java
@@ -0,0 +1,25 @@
+package day41_errors_garbagecollector;
+
+public class Errors {
+
+	public static void main(String[] args) {
+		
+		// System.out.println(20/0); // RTE degil RunTimeException
+		
+		// String str="asd; // CTE dur... cunku cift tirnagi tamamlamadikca kod calismaz
+		// bu tur yazimdan kaynaklanan hatalar kolayca duzeltilebilir.
+		
+		// Java'da Error dendiginde onune gecilemeyen, kod ile cozulemeyen
+		// sistemden kaynakli buyuk hatalar kastedilir.
+		
+		// Error'lar unchecked'dir ve Java bunlari ongoremez
+		// ancak gerceklestiginde kodun calismasi durdurulur.
+		
+		String str="";
+		for (int i = 0; i < 10000000 ; i++) {
+			str+=i;
+		}
+
+	}
+
+}
