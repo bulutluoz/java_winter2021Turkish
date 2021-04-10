2044d34d6c0951308ec5d09e98eb999b9cd2b757
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-04-10 11:54:31 +0300

    49.ders oncesi

--- src/day48_Maps/Maps02.java
+++ src/day48_Maps/Maps02.java
@@ -11,19 +11,17 @@ public class Maps02 {
 		// Asagida verilen map'de yazilim dili java olan kisi isimlerini bir liste olarak donduren
 		// bir method yaziniz
 		
-		Map<Integer,String> map1 = new HashMap<>();
-				
+		Map<Integer,String> map1 = new HashMap<>();			
 		map1.put(101, "Ali, Can, java");
 		map1.put(102, "Veli, Yan, java");
 		map1.put(103, "Ali, Yan, C#");
-		
+				
 		String istenenDil="JAVA";
 		
 		List<String> isimList = javaBilenler(map1,istenenDil);
 		System.out.println(isimList);
-
 	}
-
+	
 	private static List<String> javaBilenler(Map<Integer, String> map1, String istenenDil) {
 
 		List<String> isimListesi=new ArrayList<>();
@@ -34,12 +32,8 @@ public class Maps02 {
 			
 			if(arr[2].equalsIgnoreCase(istenenDil)) {
 				isimListesi.add(arr[0]);
-			}
-			
-		}
-		
-		
+			}	
+		}	
 		return isimListesi;
 	}
-
 }
